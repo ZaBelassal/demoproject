97dfd86b37d74f338c6a8b78ab1c1519a84365f3
Zakaria Belassal <zakariabelassal@gmail.com>
2022-06-04 14:53:17 +0000

    init 2

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,24 +1,23 @@
 import React from 'react'
-import sheet from "../../icons/sheet.png"
 import './Navbar.css'
 import {Link , NavLink} from 'react-router-dom'
-import {Button} from 'react-bootstrap'
 
 function Navbar(){
    
     return (
       
         <header className="alignImg">
-          <img src=""/>
-              <a href="#">Logo</a>
-          <nav>
-            <ul>
-              <Link className="alignItems" to="/portfolio"><img src={sheet} alt="sheet" color='secondary'/> Works</Link>
-              <Link className="alignItems" to="/resume"><img src={sheet} alt="sheet"/>Resume</Link>
-              <Link className="alignItems" to="/shelf"><img src={sheet} alt="sheet"/>Shelf</Link>
-              
-            </ul>
-          </nav>
+            <div>
+              <img src=""/>
+                  
+                  <a className="text-decoration-none fs-4" href="mailto:zakariabelassal@gmail.com"><i className="fa-solid fa-envelope"></i>zakariabelassal@gmail.com</a>
+            </div>
+            <div>
+              <Link className="bgchange text-decoration-none mx-2 fs-4" to="/portfolio"><i className="fa-solid fa-file"></i> Works</Link>
+              <Link className="text-decoration-none mx-2 fs-4" to="/resume"><i className="fa-solid fa-user-tie"></i>Resume</Link>
+              <Link className="text-decoration-none mx-2 fs-4" to="/shelf"><i className="fa-solid fa-book"></i>Shelf</Link>
+            </div>
+          
         </header>
       
     )

8324860a610e48af92d92138294676202c9b6052
Zakaria Belassal <zakariabelassal@gmail.com>
2022-06-06 03:44:40 +0000

    PrblmRotate

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,15 +9,13 @@ function Navbar(){
     return (
         <div>
         <header className="alignImg">
-            <div>
-              <img src=""/>
-                  
-                  <a className="bgchange text-decoration-none fs-4" href="mailto:zakariabelassal@gmail.com"><i className="fa-solid fa-envelope"></i>zakariabelassal@gmail.com</a>
+            <div className='marginMail'>
+                  <a className="bgchange text-decoration-none fs-4 p-2" href="mailto:zakariabelassal@gmail.com"><i className="fa-solid fa-envelope mx-2"></i>zakariabelassal@gmail.com</a>
             </div>
             <div>
-              <Link className="bgchange text-decoration-none mx-2 fs-4" to="/portfolio"><i className="fa-solid fa-file mx-2"></i> Works</Link>
-              <Link className="bgchange text-decoration-none mx-2 fs-4" to="/resume"><i className="fa-solid fa-user-tie mx-2"></i>Resume</Link>
-              <Link className="bgchange text-decoration-none mx-2 fs-4" to="/shelf"><i className="fa-solid fa-book mx-2"></i>Shelf</Link>
+              <Link className="bgchange text-decoration-none mx-2 fs-4 p-2" to="/portfolio"><i className="fa-solid fa-file mx-2"></i> Works</Link>
+              <Link className="bgchange text-decoration-none mx-2 fs-4 p-2" to="/resume"><i className="fa-solid fa-user-tie mx-2"></i>Resume</Link>
+              <Link className="bgchange text-decoration-none mx-2 fs-4 p-2" to="/shelf"><i className="fa-solid fa-book mx-2"></i>Shelf</Link>
             </div>
           
         </header>
@@ -25,19 +23,36 @@ function Navbar(){
         <section>
             <div className='container'>
               <div className='row overflow-hidden'>
-                <div className='col-md-1 my-5 py-5'>
+                <div className='col-md-1 my-5 py-5 position-relative' style={{left:"-40px",top:"160px"}}>
                     <ul>
-                      <li><i class="fa-brands fa-linkedin"></i></li>
-                      <li><i class="fa-brands fa-github"></i></li>
-                      <li><i class="fa-brands fa-twitter"></i></li>
+                    
+                      <li className='list-unstyled fs-2 px-0'>
+                          <a className="text-secondary" href="https://www.linkedin.com/in/zakaria-belassal-036a03153/" target="_blank" alt="link" title="myLinkedin">
+                          <i className="fa-brands fa-linkedin"></i></a>
+                      </li>
+
+                      <li className='list-unstyled fs-2'>
+                        <a className="text-secondary" href="https://github.com/ZaBelassal" target="_blank" alt="githubProfile">
+                          <i className="fa-brands fa-github"></i>
+                        </a>
+                      </li>
+
+                      <li className='list-unstyled fs-2'>
+                        <a className="text-secondary" href="https://twitter.com/ELMAHDI05" target="_blank" alt="githubProfile">
+                          <i className="fa-brands fa-twitter"></i>
+                        </a>
+                      </li>
                     </ul>
                 </div>
                 <div className="col-md-3 my-5 py-5">
-                    <div className='btn btn-secondary color-white fs-2 animate__animated animate__fadeInDownBig'>
+                  <div className="animate__animated animate__fadeInDownBig animationDuration">
+                    <div className='btn btn-secondary color-white fs-2'>
                       It's me
                     </div>
+                    <div className="arrow-down"></div>
+                  </div>
                     <h2 className='my-4 name'><b>Zakaria <span className='mx-5'>Belassal</span></b></h2>
-                    <div>
+                    <div className="animate__animated animate__fadeInDownBig">
                     Full Stack developer @me <br/>( JS React || Angular || Node.js || PHP Laravel )
                       <div> Javascript enthusiast</div> 
                       <div>Technologies Buffer Always learning</div> 

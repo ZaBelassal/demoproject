ddbc9d98ff6775779484d64d8fa095d2fd50a21e
Zakaria Belassal <zakariabelassal@gmail.com>
2022-06-07 23:47:18 +0000

    correctionOfNavbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import './App.css';
-import Footer from './components/Footer';
-import Navbar from './components/Navbar/Navbar'
-import { Routes, Route } from 'react-router-dom'
+import { useState } from 'react';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home'
+import {Link , NavLink ,Routes , Route} from 'react-router-dom'
 import Portfolio from './components/Portfolio/Portfolio';
 import Resume from './components/Resume/Resume';
 import Shelf from "./components/Shelf/Shelf"
 
 function App() {
+  const [mode , setMode] = useState(true)
   return (
     <div className="App">
       <div className='AppContainer'>
-        <Navbar />
+      <header className="alignImg">
+            <div className='marginMail'>
+                  <a className="bgchange text-decoration-none fs-4 p-2" href="mailto:zakariabelassal@gmail.com"><i className="fa-solid fa-envelope mx-2"></i>zakariabelassal@gmail.com</a>
+            </div>
+            <div onClick={() => setMode(prevCheck => !prevCheck)}>
+              { mode ?
+                 (
+                <i className="fa-solid fa-sun fs-4"></i>
+                )
+                :
+                (
+                <i className="fa-solid fa-moon fs-4"></i>
+                )
+
+              }
+            </div>
+            <div>
+              <Link className="bgchange text-decoration-none mx-2 fs-4 p-2" to="/portfolio"><i className="fa-solid fa-file mx-2"></i> Works</Link>
+              <Link className="bgchange text-decoration-none mx-2 fs-4 p-2" to="/resume"><i className="fa-solid fa-user-tie mx-2"></i>Resume</Link>
+              <Link className="bgchange text-decoration-none mx-2 fs-4 p-2" to="/shelf"><i className="fa-solid fa-book mx-2"></i>Shelf</Link>
+            </div>
+          
+        </header>
+
+     
         <Routes>
+            <Route exact path="/" element={<Home />} />
             <Route path="/portfolio" element={<Portfolio />} />
             <Route path="/resume" element={<Resume />} />
             <Route path="/shelf" element={<Shelf />} />
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,108 @@
-import React from 'react'
+// import React from 'react'
+import { useState } from 'react';
+import './Home.css'
+import BusinessMan from '../../icons/businessMan.png'
+import 'animate.css';
+import { useMousePosition } from "./useMouse";
 
-function Home() {
-  return (
-    <div>Bienvenue sur notre application</div>
-  )
-}
+function Home(){
 
-export default Home
+  const position = useMousePosition();
+
+        // console.log(mode)
+  // }
+  // const [MousePosition, setMousePosition] = useState({
+  //   left: 0,
+  //   top: 0
+  //   })
+  //   const cursor = document.querySelector(".cursor")
+  //   cursor.onmousemove = function(event) {
+  //   setMousePosition({
+  //   left: event.pageX,
+  //   top: event.pageY
+  //         });
+  //     }
+    
+
+  // const handleChange = (e) => {
+  //   console.log("e", e.pageX);
+  //   setMousePosition({
+  //     left: position.x,
+  //     top: position.y
+  //   });
+  // };
+
+    return (
+        <div className='overflow-hidden'>
+        <section>
+            <div className='container'>
+              <div className='row overflow-hidden'>
+                <div className='col-md-1 my-5 py-5 position-relative' style={{left:"-40px",top:"160px"}}>
+                    <ul>
+                    
+                      <li className='list-unstyled fs-2 px-0'>
+                          <a className="text-secondary" href="https://www.linkedin.com/in/zakaria-belassal-036a03153/" target="_blank" alt="link" title="myLinkedin">
+                          <i className="fa-brands fa-linkedin"></i></a>
+                      </li>
+
+                      <li className='list-unstyled fs-2'>
+                        <a className="text-secondary" href="https://github.com/ZaBelassal" target="_blank" alt="githubProfile">
+                          <i className="fa-brands fa-github"></i>
+                        </a>
+                      </li>
+
+                      <li className='list-unstyled fs-2'>
+                        <a className="text-secondary" href="https://twitter.com/ELMAHDI05" target="_blank" alt="githubProfile">
+                          <i className="fa-brands fa-twitter"></i>
+                        </a>
+                      </li>
+                    </ul>
+                </div>
+                <div className="col-md-3 my-5 py-5">
+                  <div className="animate__animated animate__heartBeat">
+                    <div className='btn btn-dark color-white fs-2 p-2'>
+                      It's me
+                    </div>
+                    <div className="arrow-down"></div>
+                  </div>
+                    <h2 className='my-4 name'><b>
+                            <span className='letterZ animate__animated animate__zoomIn delay1'>Z</span>
+                            {/* <div className="cursor" style={{left: position.x + "px",top: position.y + "px"}}></div> */}
+                            <span className='animate__animated animate__zoomIn delay2'>a</span>
+                            <span className='animate__animated animate__zoomIn delay3'>k</span>
+                            <span className='animate__animated animate__zoomIn delay4'>a</span>  
+                            <span className='animate__animated animate__zoomIn delay5'>r</span>  
+                            <span className='animate__animated animate__zoomIn delay6'>i</span> 
+                            <span className='animate__animated animate__zoomIn delay7'>a</span> 
+                            <br/>
+                            <span className='mx-5'>
+                              <span className='animate__animated animate__zoomIn delay8'>B</span>
+                              <span className='animate__animated animate__zoomIn delay9'>e</span>
+                              <span className='animate__animated animate__zoomIn delay10'>l</span>
+                              <span className='animate__animated animate__zoomIn delay11'>a</span>
+                              <span className='animate__animated animate__zoomIn delay12'>s</span>
+                              <span className='animate__animated animate__zoomIn delay13'>s</span>
+                              <span className='animate__animated animate__zoomIn delay14'>a</span>
+                              <span className='animate__animated animate__zoomIn delay15'>l</span>
+                              </span>
+                            </b>
+                            </h2>
+                    <div className="animate__animated animate__fadeInDownBig animationDelay">
+                    Full Stack developer @me <br/>( JS React || Angular || Node.js || PHP Laravel )
+                      <div> Javascript enthusiast</div> 
+                      <div>Technologies Buffer Always learning</div> 
+                      <span>Football Fan</span>
+                    </div>
+                </div>
+                <div className="col-md-8 my-5 py-5 justify-content-start">
+                    <img src={BusinessMan} alt="businessMan" height="500px" width="850px" style={{position:"absolute",zIndex:2}}/> 
+                </div>
+              </div>
+              <div className="cursor overflow-hidden" style={{left: position.x + "px",top: position.y + "px"}}></div>
+            </div>
+        </section>
+       </div>      
+    )
+  }
+  export default Home
+
